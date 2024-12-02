125068c5bcf55711d3a8fd90fa3f1d886d4680ab
Caio Busarello Dutra <contato.caiodutra08@gmail.com>
2024-12-01 23:09:23 -0300

    atualiza LabEntity para usar String como ID e adiciona anotações do Azure Cosmos

--- microservgestaolaboratorio/src/main/java/br/univille/microservgestaolaboratorio/lab/entity/LabEntity.java
+++ microservgestaolaboratorio/src/main/java/br/univille/microservgestaolaboratorio/lab/entity/LabEntity.java
@@ -1,14 +1,21 @@
 package br.univille.microservgestaolaboratorio.lab.entity;
 
 import java.io.Serializable;
-import java.util.UUID;
+
+import org.springframework.data.annotation.Id;
+
+import com.azure.spring.data.cosmos.core.mapping.Container;
+import com.azure.spring.data.cosmos.core.mapping.PartitionKey;
 
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Container(containerName = "lab", autoCreateContainer = true)
 public class LabEntity implements Serializable {
-  private UUID id;
+  @Id
+  @PartitionKey
+  private String id;
   private String name;
 }
