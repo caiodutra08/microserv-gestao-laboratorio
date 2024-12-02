e0579a80a3dd0be674c7e6c77e0147c4bd41e2a4
Caio Busarello Dutra <contato.caiodutra08@gmail.com>
2024-12-01 23:09:38 -0300

    atualiza MaintenanceEntity para usar String como ID e adiciona anotações do Azure Cosmos

--- microservgestaolaboratorio/src/main/java/br/univille/microservgestaolaboratorio/maintenance/entity/MaintenanceEntity.java
+++ microservgestaolaboratorio/src/main/java/br/univille/microservgestaolaboratorio/maintenance/entity/MaintenanceEntity.java
@@ -1,8 +1,12 @@
 package br.univille.microservgestaolaboratorio.maintenance.entity;
 
 import java.io.Serializable;
-import java.util.Date;
-import java.util.UUID;
+
+import org.springframework.data.annotation.Id;
+
+import com.azure.spring.data.cosmos.core.mapping.Container;
+import com.azure.spring.data.cosmos.core.mapping.GeneratedValue;
+import com.azure.spring.data.cosmos.core.mapping.PartitionKey;
 
 import br.univille.microservgestaolaboratorio.lab.entity.LabEntity;
 import br.univille.microservgestaolaboratorio.maintenance.enums.MaintenanceStatus;
@@ -11,11 +15,15 @@ import lombok.Setter;
 
 @Getter
 @Setter
+@Container(containerName = "maintenance", autoCreateContainer = true)
 public class MaintenanceEntity implements Serializable {
-  private UUID id;
+  @Id
+  @PartitionKey
+  @GeneratedValue
+  private String id;
   private LabEntity lab;
   private MaintenanceStatus status;
-  private Date date_maintenance;
-  private UUID id_teacher;
-  private Date created_at;
+  private String date;
+  private String teacherId;
+  private String createdAt;
 }
